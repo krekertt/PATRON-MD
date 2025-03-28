f6a19130f446639958ab6000fcfd5f0ac9172783
krekertt <timour.airatovich1@gmail.com>
2025-03-28 19:56:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 //auto presence update
 global.bankname = "MONIEPOINT"
 
-global.banknumber = "8133729715"
+global.banknumber = "79274305323"
 
-global.bankowner = "FADARE"
+global.bankowner = "Muhaj"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.wlcm = true
-global.autoblockmorroco = true //auto block 212 (true to on, false to off)
-global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
 global.antispam = false//auto kick spammer (true to on, false to off)
 global.xprefix = '.'
 
